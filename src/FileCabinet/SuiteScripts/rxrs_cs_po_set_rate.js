e6c97e73cb05b20d45cee1404d157c16e2f911d1
james <jgumapac@bspny.com>
2023-06-17 01:27:28 +0800

    Create script file that will set price level on PO the same as the sales transactioin

---
+++ src/FileCabinet/SuiteScripts/rxrs_cs_po_set_rate.js
@@ -0,0 +1,100 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType ClientScript
+ * @NModuleScope SameAccount
+ */
+define(["N/search"], function (search) {
+
+  const priceLevel = [
+    { priceName: "Mfg ERV (SysCalc)", column: "price4" },
+    { priceName: "Non-Returnable", column: "price7" },
+    { priceName: "Non-scannable", column: "price11" },
+    { priceName: "Pharma - Credit (ERV SysCalc)", column: "price10" },
+    { priceName: "Pharma - Credit (UP SysCalc)", column: "price9" },
+    { priceName: "Pharma - Credit (input)", column: "price8" },
+    { priceName: "Unit Price (input)", column: "price2" },
+    { priceName: "Online Price", column: "price5" },
+    {
+      priceName: 'Wholesale Acquisition Price '+ '"WAC"' + ' (input)',
+      column: "baseprice",
+    },
+  ];
+  function fieldChanged(scriptContext) {
+    try {
+
+      const curRec = scriptContext.currentRecord;
+      const fieldId = scriptContext.fieldId;
+      const sublistId = scriptContext.sublistId;
+
+      if (sublistId === "item") {
+
+        if (fieldId === "custcol_rxrs_price_level") {
+          let priceLevelName = curRec.getCurrentSublistText({
+            sublistId: "item",
+            fieldId: "custcol_rxrs_price_level",
+          });
+          let itemId = curRec.getCurrentSublistValue({
+            sublistId: "item",
+            fieldId: "item",
+          });
+          let rate = getItemRate({ priceLevelName, itemId });
+          rate &&
+            curRec.setCurrentSublistValue({
+              sublistId: "item",
+              fieldId: "rate",
+              value: rate
+            });
+        }
+      }
+    } catch (e) {
+      console.error("fieldChanged", e.message);
+    }
+  }
+
+  /**
+   * Get the item rate based on item id and price level name
+   * @param {string} options.priceLevelName
+   * @param {number} options.itemId
+   * @return {number} return rate of the selected price level of the item
+   */
+  function getItemRate(options) {
+    try {
+      let column = "";
+      let rate;
+      priceLevel.forEach((val) => {
+        if (val.priceName == options.priceLevelName) {
+          column = val.column;
+        }
+      });
+      let filters = [];
+      filters[0] = search.createFilter({
+        name: "internalID",
+        operator: search.Operator.IS,
+        values: options.itemId,
+      });
+      let columns =[];
+      columns[0] = search.createColumn({
+        name: column,
+      });
+
+      const itemSearch = search.create({
+        type: search.Type.ITEM,
+        filters: filters,
+        columns: columns,
+      });
+      const result = itemSearch.run();
+
+      result.each(function (row) {
+        rate = row.getValue({
+          name: column,
+        });
+      });
+      return rate
+    } catch (e) {
+      log.error("getItemRate", e.message);
+    }
+  }
+  return {
+    fieldChanged: fieldChanged,
+  };
+});
